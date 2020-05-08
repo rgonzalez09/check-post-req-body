c25345f30a1b5818b0d2cfe854f349b242ccbd0f
sandrabosk <sandraboskovic@hotmail.com>
2020-05-08 13:32:31 -0400

    add checking  for understanding

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ require('dotenv').config();
 
 const app = express();
 
-// ****************** require body-parser here: ******************
+// ****************** require the necessary npm package here: ******************
 // ... your code here
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-// *********************************************************
+
+// *****************************************************************************
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
@@ -23,26 +22,9 @@ app.get('/', (req, res) => {
 
 // 4.
 // ... your code here
-app.post('/create', (req, res) => {
-  const { userAge } = req.body;
-  console.log(`User's input: ${userAge}`);
-});
 
 // 5:
 // ... your code here
-app.post('/new/user', (req, res) => {
-  const { firstName, lastName, street, number, city, zip, country } = req.body;
-  console.log(
-    `User's input:`,
-    req.body,
-    firstName,
-    lastName,
-    street,
-    number,
-    zip,
-    country
-  );
-});
 
 app.listen(process.env.PORT, () =>
   console.log(`Running on port: ${process.env.PORT}`)
